4ad8634434a532e1d5691fb447be65cb9c74c3d0
Jacks Bruno Moran Prado <jacks.prado@grupopereira.local>
2023-03-22 13:18:48 -0400

    manipulação do DOM com js

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const controles = document.querySelectorAll('[data-controle]')
+const estatisticas = document.querySelectorAll('[data-estatisticas]')
+
+const pecas = {
+  bracos: {
+    forca: 29,
+    poder: 35,
+    energia: -21,
+    velocidade: -5
+  },
+
+  blindagem: {
+    forca: 41,
+    poder: 20,
+    energia: 0,
+    velocidade: -20
+  },
+  nucleos:{
+    forca: 0,
+    poder: 7,
+    energia: 48,
+    velocidade: -24
+  },
+  pernas:{
+    forca: 27,
+    poder: 21,
+    energia: -32,
+    velocidade: 42
+  },
+  foguetes:{
+    forca: 0,
+    poder: 28,
+    energia: 0,
+    velocidade: -2
+  }
+}
+
+controles.forEach(e => {
+  e.addEventListener('click', event => {
+    manipulaDados(event.target.dataset.controle, event.target.parentNode)
+    atualizaEstatistica(event.target.dataset.peca)
+  })
+})
+
+function manipulaDados(operador, controle){
+  const peca = controle.querySelector('[data-contador]')
+  if(operador=='-'){
+    peca.value = parseInt(peca.value) - 1
+  }else{
+    peca.value = parseInt(peca.value) + 1
+  }
+}
+
+function atualizaEstatistica(peca){
+  estatisticas.forEach(e => {
+    e.textContent = parseInt(e.textContent) + pecas[peca][e.dataset.estatisticas]
+  })
+}
